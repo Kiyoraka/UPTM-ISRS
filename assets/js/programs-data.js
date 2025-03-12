fd504793e434c5bdda0594fc7d2f161a9744a21d
Afif Maahi <94098971+Kiyoraka@users.noreply.github.com>
2025-03-12 16:33:15 +0800

    updated program data

--- assets/js/programs-data.js
+++ assets/js/programs-data.js
@@ -1,38 +1,37 @@
-/**
- * Program data organized by academic levels
- */
 const programsData = {
     "bachelors": [
-        { code: "BAC", name: "Bachelor of Accountancy (Honours)" },
-        { code: "BBAHRM", name: "Bachelor of Business Administration (Honours) in Human Resource Management" },
-        { code: "BBA", name: "Bachelor of Business Administration (Honours)" },
-        { code: "BCC", name: "Bachelor of Communication (Honours) in Corporate Communication" },
-        { code: "BBAH", name: "Bachelor of Business Administration (Hybrid)" },
-        { code: "BAAELS", name: "Bachelor of Arts (Honours) in Applied English Language Studies" },
-        { code: "BECE", name: "Bachelor of Early Childhood Education (Honours)" },
-        { code: "BEDTESL", name: "Bachelor of Education (Honours) in Teaching English as a Second Language (TESL)" },
-        { code: "BCA", name: "Bachelor of Corporate Administration (Honours)" },
-        { code: "BA3D", name: "Bachelor of Arts in 3D Animation and Digital Media (Honours)" },
-        { code: "BITBC", name: "Bachelor of Information Technology (Honours) in Business Computing" },
-        { code: "BITCAD", name: "Bachelor of Information Technology (Honours) in Computer Application Development" },
-        { code: "BITCS", name: "Bachelor of Information Technology (Honours) in Cyber Security" }
+        { code: "AA201", name: "Bachelor of Accountancy (Honours)" },
+        { code: "AB202", name: "Bachelor of Business Administration (Honours)" },
+        { code: "AB203", name: "Bachelor in Business Administration (Digital Marketing)(Honours)" },
+        { code: "AB204", name: "Bachelor of Business Administration (Honours) Human Resource Management" },
+        { code: "AC201", name: "Bachelor of Corporate Administration (Honours)" },
+        { code: "BE201", name: "Bachelor of Arts (Honours) in Applied English Language Studies" },
+        { code: "BE202", name: "Bachelor of Early Childhood Education (Honours)" },
+        { code: "BE203", name: "Bachelor of Education (Honours) in Teaching English as a Second Language" },
+        { code: "BK201", name: "Bachelor of Communication (Hons) in Corporate Communication" },
+        { code: "CT203", name: "Bachelor in Computer Science (Honours)" },
+        { code: "CT203", name: "Bachelor of Information Technology (Honours) in Business Computing" },
+        { code: "CT204", name: "Bachelor of Information Technology (Honours) in Computer Application Development" },
+        { code: "CT206", name: "Bachelor of Information Technology (Honours) in Cyber Security" },
+        { code: "CM201", name: "Bachelor of Arts in 3D Animation and Digital Media (Honours)" }
     ],
     "masters": [
-        { code: "MSIS", name: "Master of Science in Information Systems" },
-        { code: "MBA", name: "Master of Business Administration (in collaboration with CMI)" },
-        { code: "MBACAG", name: "MBA (Corporate Administration and Governance) (in collaboration with MAICSA)" },
-        { code: "MAcc", name: "Master of Accountancy (in collaboration with CIMA)" }
+        { code: "AA301", name: "Master of Accountancy" },
+        { code: "AB301", name: "Master of Business Administration" },
+        { code: "AB302", name: "Master of Business Administration (Corporate Administration and Governance)" },
+        { code: "AB303", name: "Master of Business Information Systems" },
+        { code: "BE303", name: "Master of Education (TESL)" }
     ],
     "phd": [
-        { code: "PhDBA", name: "Doctor of Philosophy in Business Administration" },
-        { code: "PhDIT", name: "Doctor of Philosophy in Information Technology" },
-        { code: "PhDEd", name: "Doctor of Philosophy in Education" }
+        { code: "AA401", name: "Doctor of Philosophy (Business Administration)" },
+        { code: "BE401", name: "Doctor of Philosophy (Education)" },
+        { code: "CT401", name: "Doctor of Philosophy (Information Technology)" }
     ]
 };
 
 // Level labels for the optgroups
 const programLevelLabels = {
     "bachelors": "Bachelor's Degree",
-    "masters": "Master",
-    "phd": "Doctor of Philosophy"
+    "masters": "Master's Degree",
+    "phd": "Doctor of Philosophy",
 };
